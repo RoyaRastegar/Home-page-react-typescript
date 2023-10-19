146ab4020dfcc420d4c3fe19aa94ed70735d71af
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2023-10-19 22:53:19 +0800

    Add navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,4 +1,74 @@
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
-  return <div>navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handelDropDown() {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  }
+  return (
+    <>
+      <div className="navbar">
+        <div className="logo">
+          <a href="#">Software Recruitment co.</a>
+        </div>
+        <ul className="links">
+          <li>
+            <a href="jobseekers">For jobseekers</a>
+          </li>
+          <li>
+            <a href="client">For clients</a>
+          </li>
+          <li>
+            <a href="sector">Sectors</a>
+          </li>
+          <li>
+            <a href="resources">Resources</a>
+          </li>
+        </ul>
+        <div className="btn">
+          <a href="#" className="action_btn_cv">
+            Uploud cv
+          </a>
+          <a href="#" className="action_btn_contact">
+            Contact us
+          </a>
+        </div>
+        <button onClick={handelDropDown} className="toggle_btn">
+          {isOpen ? (
+            <FontAwesomeIcon icon={faXmark} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
+        </button>
+      </div>
+      {isOpen && (
+        <div className="dropdown_menu open">
+          <li>
+            <a href="jobseekers">For jobseekers</a>
+          </li>
+          <li>
+            <a href="client">For clients</a>
+          </li>
+          <li>
+            <a href="sector">Sectors</a>
+          </li>
+          <li>
+            <a href="resources">Resources</a>
+          </li>
+          <a href="#" className="action_btn_cv">
+            Uploud cv
+          </a>
+          <a href="#" className="action_btn_contact">
+            Contact us
+          </a>
+        </div>
+      )}
+    </>
+  );
 };
 export default Navbar;
