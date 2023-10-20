f26ea9b1323865430997eda7bfd44677f5d1d6ae
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2023-10-20 11:54:09 +0800

    update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Carrer from "./Components/Carrer/Carrer";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      {" "}
       <Navbar />
-    </div>
+      <Carrer />
+    </>
   );
 }
 
